f4ca3b8cbd8c933bc0fc89ff034052f3ed5bfbfc
robitu <robertoiturralde2009@hotmail.com>
2023-07-24 17:56:44 -0600

    Added Mongoose Database

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -6,18 +6,18 @@ import { useState, useEffect } from "react";
 import { signIn, signOut, useSession, getProviders } from "next-auth/react";
 
 const Nav = () => {
-  const isUserLogged = true;
-
+  const { data: session } = useSession();
   const [providers, setProviders] = useState(null);
 
   const [toggleDropDown, setToggleDropDown] = useState(false);
 
   useEffect(() => {
-    const setProviders = async () => {
+    const fetchProviders = async () => {
       const response = await getProviders();
       setProviders(response);
     };
-  });
+    fetchProviders();
+  }, []);
   return (
     <nav className="flex-between w-full mb-16 pt-3">
       <Link href="/" className="flex gap-2 flex-center">
@@ -30,9 +30,8 @@ const Nav = () => {
         />
         <p className="logo_text">Promptopia</p>
       </Link>
-
       <div className="sm:flex hidden">
-        {isUserLogged ? (
+        {session?.user ? (
           <div className="flex gap-3 md:gap-5">
             <Link href="/create-prompt" className="black_btn">
               {" "}
@@ -43,7 +42,7 @@ const Nav = () => {
             </button>
             <Link href="/profile">
               <Image
-                src="/assets/images/logo.svg"
+                src={session?.user.image}
                 width={37}
                 height={37}
                 className="rounded-full"
@@ -59,8 +58,10 @@ const Nav = () => {
                   type="button"
                   key={provider.name}
                   onClick={() => signIn(provider.id)}
-                  className="btn-black"
-                ></button>
+                  className="black_btn"
+                >
+                  Sign In
+                </button>
               ))}
           </>
         )}
@@ -68,10 +69,10 @@ const Nav = () => {
 
       {/** Mobile Navigation */}
       <div className="sm:hidden flex relative">
-        {isUserLogged ? (
+        {session?.user ? (
           <div className="flex">
             <Image
-              src="/assets/images/logo.svg"
+              src={session?.user.image}
               width={37}
               height={37}
               className="rounded-full"
@@ -116,8 +117,10 @@ const Nav = () => {
                   type="button"
                   key={provider.name}
                   onClick={() => signIn(provider.id)}
-                  className="btn-black"
-                ></button>
+                  className="black_btn"
+                >
+                  Sign In
+                </button>
               ))}
           </>
         )}
